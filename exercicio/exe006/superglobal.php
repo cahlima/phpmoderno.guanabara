d31b4a1787212a42f6c33e3b5a25e78428072624
AcaciaLima <caciabad@gmail.com>
2023-10-04 17:03:26 -0300

    04/10 17:00

--- exercicio/exe006/superglobal.php
+++ exercicio/exe006/superglobal.php
@@ -11,12 +11,34 @@
         <main>
             <pre>
                     <?php
+                        setcookie("dia-da-semana", "SEGUNDA", time() + 3600);        
+                        
+                        session_start();
+                        $_SESSION["teste"] = "FUNCIONOU";
+
+                        
+
+                    echo "<h1>Superglobal GET </h1>";
+                    var_dump($_GET);
                     
+                    echo "<h1> Superglobal POST</h1>";
+                    var_dump($_POST);
                     
+                    echo "<h1> Superglobal REQUEST</h1>";
+                    var_dump($_REQUEST);
+
+                    echo "<h1> Superglobal COOKIE</h1>";
+                    var_dump($_COOKIE);
+
+                    echo "<h1> Superglobal SESSION</h1>";
+                    var_dump($_SESSION);
                     
-                    
-                    
-                    
+                    echo "<h1> Superglobal ENV</h1>";
+                    var_dump($_ENV);
+
+                    echo "<h1> Superglobal SERVER</h1>";
+                    var_dump($_SERVER);
+
                     ?>
 
 
@@ -29,6 +51,5 @@
 
         </main>
 
-
-
 </body>
+</html>
