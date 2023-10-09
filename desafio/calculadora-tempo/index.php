3e6bd6429b8b82cba1497ab5afc84f79f622b5d8
AcaciaLima <caciabad@gmail.com>
2023-10-09 20:33:38 -0300

    09/10 20:33

--- desafio/calculadora-tempo/index.php
+++ desafio/calculadora-tempo/index.php
@@ -11,13 +11,8 @@
 
     <?php
 //AREA DE DECLARAÇÕES
-              $num =$_REQUEST['num'];
-              $seg = 1;
-              $min =  60;
-              $hor = 3600  ; 
-              $dia = 86400;
-              $semana = 604800;
-
+              $total = $_REQUEST['seg']?? 0;
+             
     ?>
  
 <main>
@@ -25,33 +20,44 @@
                    <h2>Calculadora de Tempo</h2>
             <form action="<?=@$_SERVER ['PHP_SELF']?> " method="request">
                 <label for="num">Qual o total de segundos?</label>
-                <input type="number" name="num" id="num" min="1" step="1" value="<?=$num?>">
+                <input type="number" name="seg" id="seg" min="0" step="1" required value="<?=$total?>">
 
                 <input type="submit"  value="Calcular">
             </form>
     </main>
 
-    <section>
-        <h1>Totalizando tudo</h1>
+  
 
     <?php
 
-    $seg = 1;
-    $min = $seg * 60; 
-    $hor = $min * 60; 
-    $dia = $hor * 24;
-    $sem = $dia *7;
-   
 
-    echo "<p>Analisando o valor que você digiotu $num segundos</strong> equivalem a um total de:";
-   
-    echo "<ul><li> $sem semanas </li>";
-    echo "<li> $dia dias </li>";
-    echo "<li> $hor horas </li>";
-    echo "<li> $min minutos </li>";
-    echo "<li> $seg segundos </li>";
+        $sobra =$total;
+        //Total de Semanas
+        $semana = (int)($sobra / 604_800);
+        $sobra = $sobra % 604_800;
+        // Total de Dias
+        $dia = (int)($sobra / 86_400);
+        $sobra = $sobra % 86_400;
+        // Total de Horas
+        $hora = (int)($sobra / 3_600);
+        $sobra = $sobra % 3_600;
+        // Total de MInutos
+        $minuto = (int)($sobra / 60);
+        $sobra = $sobra % 60;
+        // Total de Segundo
+               $segundo = $sobra 
       ?>
-    
+
+<section>
+        <h1>Totalizando tudo</h1>
+        <p>Analisando o valor que você digiotu <strong> <?=$total?> segundos</strong> equivalem a um total de:
+   
+   <li> <?=$semana?> semanas </li>
+   <li> <?=$dia?> dias </li>
+   <li> <?=$hora?> horas </li>
+   <li> <?=$minuto?> minutos </li>
+  <li> <?=$segundo?> segundos </li>
+
     </section>
     </body>
 </html>
